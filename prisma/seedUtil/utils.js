da945981d74bbf0135bdc8da9c29a2c83dcd086e
Eriberto <eriberto.guzman@outlook.com>
2023-04-23 12:29:59 -0400

    small change to the utils

--- prisma/seedUtil/utils.js
+++ prisma/seedUtil/utils.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 const { getUsData } = require("../../lib/data/util/download");
 
 const usWriteBabyFiles = () => {
+  if (filesExists())
+    return console.log("Data Already Exists: Check data folder");
+  console.log("Gathering data...🗄️🗃️📂...📁...📂🗃️🗄️...");
+  getUsData();
+};
+
+const filesExists = () => {
   const usFolder = "./lib/data/us";
   fs.readdir(usFolder, (err, files) => {
-    if (files && files.length > 0)
-      return console.log("Data Already Exists: Check data folder");
-    getUsData();
+    if (files && files.length > 0) {
+      return true;
+    }
+    return false;
   });
 };
 
